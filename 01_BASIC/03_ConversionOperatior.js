a5692138002f528d27555ef0c44825e02a6ad007
Codehelpaman <204889743+CodeHelpAman@users.noreply.github.com>
2025-07-24 19:04:39 +0530

    Update 03_ConversionOperatior.js

--- 01_BASIC/03_ConversionOperatior.js
+++ 01_BASIC/03_ConversionOperatior.js
@@ -1,7 +1,8 @@
-let score = "33";
+// ************************ Conversion ****************************
+// let score = "33";
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 // let valueInNumber = Number(score);
 
@@ -12,7 +13,7 @@ console.log(typeof score);
 // "33abc" => NaN
 // true => 1, false => 0
 
-let inLoggend = "aman";
+// let inLoggend = "aman";
 
 // let BooleaninLoggend = Boolean(inLoggend);
 // console.log(typeof BooleaninLoggend);
@@ -22,8 +23,18 @@ let inLoggend = "aman";
 // 0 =>false , 1 => true
 // "" => false
 
-let SomeNumber = 33;
+// let SomeNumber = 33;
 
-let StringNumber = String(SomeNumber);
-console.log(typeof StringNumber);
-console.log(StringNumber);
+// let StringNumber = String(SomeNumber);
+// console.log(typeof StringNumber);
+// console.log(StringNumber);
+
+// **************************** Operation ******************************************
+
+// let Value = 3;
+// let Negvalue = -Value;
+// console.log(Negvalue);
+
+let str1 = "hello";
+let str2 = "Aman";
+let str3 = "str1 + str2";
