b4be515beb5968cba94dd976e78f481a45a0955e
Codehelpaman <204889743+CodeHelpAman@users.noreply.github.com>
2025-07-24 18:41:52 +0530

    Create 03_ConversionOperatior.js
    ConversionOption

---
+++ 01_BASIC/03_ConversionOperatior.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let inLoggend = "aman";
+
+// let BooleaninLoggend = Boolean(inLoggend);
+// console.log(typeof BooleaninLoggend);
+// console.log(BooleaninLoggend);
+
+// true => "aman"
+// 0 =>false , 1 => true
+// "" => false
+
+let SomeNumber = 33;
+
+let StringNumber = String(SomeNumber);
+console.log(typeof StringNumber);
+console.log(StringNumber);
