88a35005039d9a83b42f0f19aa5af0dde137c3a3
Codehelpaman <204889743+CodeHelpAman@users.noreply.github.com>
2025-07-24 15:58:22 +0530

    Create 02_Datatype.js

---
+++ 01_BASIC/02_Datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version.
+
+// alert( 3 + 3 ); // we are not using node.js Not Browser
+
+console.log(3 + 3);
+console.log("Amanyadav");
+
+// data types
+// 1. Numbers => 2 to power 53
+// 2. Bigint
+// 3. String => ""
+// Boolean => true/false
+// Null => standalone value
+// Undefined => Undefined
+// Symbol => Unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
